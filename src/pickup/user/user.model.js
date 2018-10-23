faf3cf8563dac1dff9bd0b2eb8844c2117699f3a
sskelkar <skelkar@equalexperts.com>
2018-10-23 18:20:05 +0530

    user should return id without underscore

--- src/pickup/user/user.model.js
+++ src/pickup/user/user.model.js
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 
-let UserSchema = mongoose.Schema({
-    id: {type: String},
+let userSchema = mongoose.Schema({
     name: {type: String, required: true},
     phone: {type: String, required: true},
     role: {type: String, enum: ['RESIDENT', 'COLLECTOR'], required: true},
     karmaPoints: {type: Number, default: 0}
 });
-
-const User = mongoose.model('User', UserSchema);
+userSchema.virtual('id', () => this._id.toHexString());
+userSchema.set('toJSON', {
+    virtuals: true
+});
+const User = mongoose.model('User', userSchema);
 export default User;
